0f6d368945df62b1762b4f2fe8bce21e51826444
Amikom <salsabilasyifa4091@gmail.com>
2023-12-09 20:38:41 +0700

    catatan : 1. belum bisa login 2. fragment order belum selesai 3. fragment Favorite belum selesai 4. klik motor dan destinasi belum selesai juga

---
+++ app/src/main/java/com/example/rental/AdapterHistory.kt
@@ -0,0 +1,5 @@
+package com.example.rental
+
+class AdapterHistory(var img:Int, var tanggal:String, var title:String, var harga:String,
+                     var pemesan:String, var antar:String, var jemput:String) {
+}
