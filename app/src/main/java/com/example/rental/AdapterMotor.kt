e039ae4e82e334c7ec5bef70b4c0c41dc53a2361
ssalsabila0911 <salsabilasyifa4091@gmail.com>
2023-12-17 22:55:49 +0700

    db motor

--- app/src/main/java/com/example/rental/AdapterMotor.kt
+++ app/src/main/java/com/example/rental/AdapterMotor.kt
@@ -5,45 +5,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterMotor(private val listMotor:List<MotorModel>): RecyclerView.Adapter<AdapterMotor.ViewHolder>() {
+class AdapterMotor(private val listMotor: List<MotorModel>) : RecyclerView.Adapter<AdapterMotor.ViewHolder>() {
 
-    class ViewHolder(ItemView: View):RecyclerView.ViewHolder(ItemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textNoStnk: TextView = itemView.findViewById(R.id.textNoStnk)
+        val imageMotor: ImageView = itemView.findViewById(R.id.imageViewMotor)
+        val textTitle: TextView = itemView.findViewById(R.id.textNam)
+        val textDesc: TextView = itemView.findViewById(R.id.textDes)
 
-        init {
-            val textStnk:TextView = ItemView.findViewById(R.id.textNoStnk)
-            val image:ImageView = ItemView.findViewById(R.id.imageViewMotor)
-            val title:TextView = ItemView.findViewById(R.id.textNam)
-            val desc:TextView = ItemView.findViewById(R.id.textDes)
-        }
-
-        fun bind(data: MotorModel){
-            val nostnk:String = data.nostnk
-            val image:String = data.gambar
-            val title:String = data.merktipe
-            val desc:String = data.kondisi + " - " + data.tahun + " - " + data.harga
-
+        fun bind(data: MotorModel) {
+            textNoStnk.text = data.nostnk
+            val imageResId = itemView.context.resources.getIdentifier(data.gambar, "drawable", itemView.context.packageName)
+            imageMotor.setImageResource(imageResId)
+            textTitle.text = data.merktipe
+            textDesc.text = "${data.kondisi} - ${data.tahun} - ${data.harga}"
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.card_layout_motor,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_layout_motor, parent, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
-       holder.bind(listMotor[position])
-
-        //holder.textStnk.text = modelMotor.nostnk
-        //holder.image.setImageURI(Uri.parse("@drawable/" + modelMotor.gambar))
-        //holder.title.text = modelMotor.merktipe
-        //holder.desc.text = modelMotor.kondisi + " - " + modelMotor.tahun + " - " + modelMotor.harga
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listMotor[position])
     }
 
-
     override fun getItemCount(): Int {
-       return listMotor.size
+        return listMotor.size
     }
 }
