026322c799e109e49abb6068c4afa3122934cc9b
Amikom <salsabilasyifa4091@gmail.com>
2023-12-20 10:43:30 +0700

    18 12 2023

---
+++ app/src/main/java/com/example/rental/EditAkunActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rental
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditAkunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_akun)
+    }
+}
